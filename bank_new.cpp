4fb2d3eeb4f22dcc2289ce6eef3cd7f8feb28269
davillbert <56187467+davillbert@users.noreply.github.com>
2021-03-04 22:57:05 +0000

    Update bank_new.cpp

--- bank_new.cpp
+++ bank_new.cpp
@@ -1,22 +1,8 @@
 #include <iostream>
 using namespace std;
-class Person
-{
-	//умеет давать информацию о ячейке, которой владеет - геттер
-	//умеет давать информацию жив или мерт (в зони видимости или нет), дэлитнули или нет, создали или нет
-	//умеет умирать
-	//умеет получать ячейку, конструктор
-public:
-	shared_ptr<BankCell> BC = nullptr(new BankCell); //даем челу ячейку
-
-	Person(shared_ptr<BankCell> BC ) : BC(BC) { cout << "Person open Bank Cell" << endl; } //присваиваем челу ячейку
-
-	shared_ptr<BankCell> getBankCell() { return BC; };
 
 
-	void setBankCell(shared_ptr<BankCell> BC_) { this->BC = BC_; } //даем ячейку
 
-};
 
 class BankCell //мб зафрендить
 {
@@ -28,9 +14,35 @@ public:
 	~BankCell() { std::cout << "BankCell destroyed\n"; }
 };
 
+class Person
+{
+	//умеет давать информацию о ячейке, которой владеет - геттер
+	//умеет давать информацию жив или мерт (в зони видимости или нет), дэлитнули или нет, создали или нет
+	//умеет умирать
+	//умеет получать ячейку, конструктор
+public:
+
+	BankCell BC;
+	
+
+	Person(BankCell BC) : BC(BC) { cout << "Person open Bank Cell" << endl; } //присваиваем челу ячейку
+	
+	
+	std::shared_ptr<BankCell> Piece(BC); //даем челу ячейку
+
+	std::shared_ptr<BankCell> getBankCell() { return& Piece; };
+
+	//BankCell& BankCell::operator =(const BankCell&)
+
+	void setBankCell(std::shared_ptr<BankCell> BC_) { this->BC = BC_; } //даем ячейку
+
+};
+
+
+
 int main()
 {
-	BankCell Exem1;
-	Person Alisa(Exem1), Bob(Exem1);
-
+	BankCell Exem1; //создали ячейку
+	Person Alisa(Exem1), Bob(Exem1); // присвоили
+	return 0;
 }
